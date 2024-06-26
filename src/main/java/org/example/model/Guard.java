cb4690fd155da76c8e8a01b074f4e773b668b974
Maminiainaando <hei.maminiaina.444@gmail.com>
2024-06-26 22:29:11 +0200

    fixed:method salaryTotal's Guard

--- src/main/java/org/example/model/Guard.java
+++ src/main/java/org/example/model/Guard.java
@@ -5,30 +5,79 @@ import lombok.Setter;
 import lombok.ToString;
 
 import java.util.Date;
+import java.util.List;
+
 @Getter
 @Setter
 @ToString
-
-final class Guard extends Employee{
+public final class Guard extends Employee{
+    private final String nameCategorie;
     private static final double TAUX_HS30 = 1.3;
     private static final double TAUX_HS50 = 1.5;
-
-    public Guard(String nom, String prenom, int numeroMatricule, Date dateDeNaissance, Date dateDEmbauche, Date dateFinContrat, Double salaireBrute, String categorie) {
-        super(nom, prenom, numeroMatricule, dateDeNaissance, dateDEmbauche, dateFinContrat, salaireBrute, categorie);
+    private static final double HEUREMAJORE_HM30 = 1.3;
+    private static final double HEUREMAJORE_HM40 = 1.4;
+    private static final double HEUREMAJORE_HM50 =1.5;
+    private String jourOuNuit;
+    public Guard(String nom, String prenom,int numeroMatricule, Date dateDeNaissance, Date dateDEmbauche, Date dateFinContrat, double salaireBrute, String nameCategorie,String jourOuNuit) {
+        super(nom, prenom, numeroMatricule, dateDeNaissance, dateDEmbauche, dateFinContrat, salaireBrute);
+        this.nameCategorie = nameCategorie;
+        this.jourOuNuit=jourOuNuit;
     }
 
     @Override
-    public double calculerSalaireTotal(int heuresNormales, int heuresSupplementaires, int heuresMajorees) {
-        double salaireNormal = this.getSalaireBrute();
-        double salaireSupplementaire = 0.0;
+    public double calculerSalaireTotal(int heuresNormales,int heuresSupplementaires,int heuresMajorees) {
+        double result=0;
+        double salaireUneHeure=salaireNormalParDesHeureNormalParSemaine()/56d;
+        double salaireHeureNormal=0;
+        double salaireSupplementaire=0;
+        double salaireMajore=0;
+        salaireHeureNormal=heuresNormales*salaireUneHeure;
+        result=salaireHeureNormal;
+        return result;
 
-        if (heuresSupplementaires > 8) {
-            salaireSupplementaire += 8 * (salaireNormal / heuresNormales) * TAUX_HS30;
-            salaireSupplementaire += (heuresSupplementaires - 8) * (salaireNormal / heuresNormales) * TAUX_HS50;
-        } else {
-            salaireSupplementaire += heuresSupplementaires * (salaireNormal / heuresNormales) * TAUX_HS30;
+    }
+
+    public double calculerSalaireTotalEnUtilisantHeure(int heuresNormales,int heuresSupplementaires,int heuresMajoreNuit,int heuresMajoreDimanche,int heuresMajoreFerie) {
+        double result=0;
+        double salaireDeUneHeure=salaireNormalParDesHeureNormalParSemaine()/nombreHeureNormalDeTravailParSemaine();
+        double salaireUnJour=salaireNormalParDesHeureNormalParSemaine()/7;
+        double salaireUneNuit=130000/7;
+        double salaireNormal=heuresNormales*salaireDeUneHeure;
+        double salaireSupplementaire=0;
+        if(heuresMajoreNuit>0 && heuresMajoreFerie==0){
+            result = salaireNormal * HEUREMAJORE_HM30;
+        } else if (heuresMajoreFerie>0 && heuresMajoreNuit==0) {
+            result = salaireNormal+(heuresMajoreFerie*salaireUnJour*HEUREMAJORE_HM30);
+        }else if(heuresMajoreFerie>0 && heuresMajoreNuit>0){
+            System.out.println(salaireUneNuit);
+            result = salaireNormal * HEUREMAJORE_HM30;
+            result +=heuresMajoreFerie*salaireUneNuit*HEUREMAJORE_HM50;
+        }else {
+            result=salaireNormal;
         }
+        System.out.println("Salaire de une heure : "+salaireDeUneHeure);
+        System.out.println("Salaire de une nuit : "+salaireUneNuit);
+        System.out.println("Salaire normal : "+salaireNormal);
+        return result;
+    }
+    @Override
+    public int nombreHeureNormalDeTravailParSemaine() {
+        return 70;
+    }
 
-        return salaireNormal + salaireSupplementaire;
+    public Double salaireNormalParDesHeureNormalParSemaine() {
+        return 100000d;
+    }
+    public Double calculSalaireHeureMajoreNuit(){
+        return salaireNormalParDesHeureNormalParSemaine()*HEUREMAJORE_HM30;
+    }
+    public Double calculSalaireHeureMajoreDimanche(){
+        return salaireNormalParDesHeureNormalParSemaine()*HEUREMAJORE_HM40;
+    }
+    public Double calculSalaireHeureMajoreFerie(){
+        return salaireNormalParDesHeureNormalParSemaine()*HEUREMAJORE_HM50;
+    }
+    public Double montantIndemnitéProportionnelleAuSalaire() {
+        return null;
     }
 }
